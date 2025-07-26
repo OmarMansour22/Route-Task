101eece830a69d73586cbe21e6d599d1ea0b0b79
OmarMansour22 <omarmansour996@gmail.com>
2025-07-26 07:17:04 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 class="text-3xl font-bold underline text-red-400">
+        Hello world! <i className="fas fa-spinner fa-spin"></i>
+      </h1>
+    </>
+  );
+}
+
+export default App;
