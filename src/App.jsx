1b1cdd744766fd26c8351c13ea0810c4f09c7131
OmarMansour22 <omarmansour996@gmail.com>
2025-07-27 21:20:47 +0300

    feat: create project routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,40 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
+import Layout from "./Components/Layout/Layout";
+import Home from "./Pages/Home/Home";
+import Products from "./Pages/Products/Products";
+import ProductDetails from "./Pages/ProductDetails/ProductDetails";
+import NotFound from "./Pages/NotFound/NotFound";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ToastContainer } from "react-toastify";
+
+const router = createBrowserRouter([
+  {
+    path: "",
+    element: <Layout />,
+    children: [
+      { index: true, element: <Home /> },
+      {
+        path: "products",
+        element: <Products />,
+      },
+      {
+        path: "productDetails/:id",
+        element: <ProductDetails />,
+      },
+      { path: "*", element: <NotFound /> },
+    ],
+  },
+]);
+
+let queryClient = new QueryClient();
 
 function App() {
   return (
     <>
-      <h1 class="text-3xl font-bold underline text-red-400">
-        Hello world! <i className="fas fa-spinner fa-spin"></i>
-      </h1>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} /> <ToastContainer />
+      </QueryClientProvider>
     </>
   );
 }
